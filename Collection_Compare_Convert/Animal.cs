fc3ac686cbf21f658ede81f136b6991aaed0c1a2
Zhongyi Wang <wang_zhyi@126.com>
2016-05-13 14:12:51 +0800

    add three src file

---
+++ Collection_Compare_Convert/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public abstract class Animal
+    {
+        protected string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public Animal() // default constructor
+        {
+            name = "The animal with no name";
+        }
+
+        public Animal(string newName)
+        {
+            name = newName;
+        }
+        public void Feed()
+        {
+            Console.WriteLine("{0} hase been fed.", name);
+        }
+    }
+}
---
+++ Collection_Compare_Convert/Chicken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Chicken : Animal
+    {
+        public void LayEgg()
+        {
+            Console.WriteLine("{0} has laid a egg.", name);
+        }
+        public Chicken(string newName) : base(newName)
+        {
+
+        }
+    }
+}
