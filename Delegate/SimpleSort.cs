82c6550ce745d9f781a5b21c0688faf76ff8c0ca
Zhongyi Wang <wang_zhyi@126.com>
2016-05-26 15:59:29 +0800

    add 2 files: simpleSort and UseSimpleSort, which contains bubble sort for vale type

---
+++ Delegate/SimpleSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public enum SortType
+    {
+        Ascending,
+        Descending
+    }
+    class SimpleSort
+    {
+
+        public void BubbleSort<T>(T[] items, SortType sortOrder)
+            where T: struct,IComparable
+        {
+            int i, n;
+            T temp;
+            bool sorted = false;
+
+            n = items.Length;
+            while(!sorted)
+            {
+                sorted = true;
+                for (i=1; i<n; i++)
+                {
+                    switch (sortOrder)
+                    {
+                        case SortType.Ascending:
+                            if (items[i-1].CompareTo(items[i]) > 0)
+                            {
+                                temp = items[i-1];
+                                items[i-1] = items[i];
+                                items[i] = temp;
+                                sorted = false;
+                            }
+                            break;
+                        case SortType.Descending:
+                            if (items[i-1].CompareTo(items[i]) < 0)
+                            {
+                                temp = items[i-1];
+                                items[i-1] = items[i];
+                                items[i] = temp;
+                                sorted = false;
+                            }
+                            break;
+                    }
+                }
+                n--;
+            }
+        }
+    }
+}
---
+++ Delegate/UseSimpleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    class UseSimpleSort
+    {
+        public static void Main(string[] args)
+        {
+            double[] intArray = new double[10] { 23, 34, 56, 12, 2, 3, 67, 82, 12, 8 };
+            SimpleSort simpleSort = new SimpleSort();
+            
+            Console.WriteLine("Sorted by Ascending:");
+            simpleSort.BubbleSort(intArray, SortType.Ascending);
+            foreach (double item in intArray)
+                Console.Write("{0}, ", item);
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted by Descending:");
+            simpleSort.BubbleSort(intArray, SortType.Descending);
+            foreach (double item in intArray)
+                Console.Write("{0}, ", item);
+            Console.WriteLine();
+
+        }
+    }
+}
